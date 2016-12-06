d782d27f129e99980fc0eb615049bb3e381d6528
Maciej Nabozny <maciej.nabozny@cloudover.io>
2016-12-06 11:08:45 +0100

    Prepared for 16.12

--- scripts/core/corecluster_config.py
+++ scripts/core/corecluster_config.py
@@ -1,11 +1,10 @@
-######################################
-# Basic setup                        #
-######################################
-
-# Should be only UI(s) host(s)
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
+    'logs': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/var/lib/cloudOver/logs.sqlite3',
+    },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cloudover',
@@ -17,60 +16,48 @@ DATABASES = {
             "init_command": "SET NAMES utf8",
         }
     },
-    'logs': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/lib/cloudOver/logs.sqlite3',
-    },
 }
 
 CACHE_URL = 'localhost'
+
 DNS_DOMAIN = 'localdomain'
-VNC_ADDRESS = '127.0.0.1'
+
+VNC_ADDRESS = '192.168.26.254'
 VNC_PORTS = {
     'START': 5900,
     'EXCLUDE': []
 }
+
 AUTOREGISTER_NODE = True
 
-######################################
-# User accounts                      #
-######################################
 USER_QUOTA = {
-    'cpu': 30,
-    'memory': 80000,                # in MB
-    'storage': 80*1024*1024*1024,     # in bytes
-    'redirections': 5,              # Public IP redirections
+    'cpu': 20,
+    'memory': 40000,                # in MB
+    'storage': 40000*1024*1024,     # in bytes
+    'redirections': 1,              # Public IP redirections
     'points': 2000,
-    'public_lease': 2,
-    'routed_lease': 128,
-    'network_isolated': 16,
+    'public_lease': 5,
+    'routed_lease': 64,
+    'network_isolated': 10
 }
 
 ENABLE_ALL_PERMISSIONS = True
 CREATE_ADDITIONAL_ROLES = True
 CHECK_STATES = False
-
-######################################
-# Advanced options                   #
-######################################
-MAX_UPLOAD_CHUNK_SIZE = 1024*1024*100
+MAX_UPLOAD_CHUNK_SIZE = 1024*1024*10
 NODE_SUSPEND_DURATION = 60 * 60 * 24
 NODE_WAKEUP_TIME = 30
 SECRET_KEY = '_SECRET_KEY'
-
-NODE_SELECT_ALGORITHM = 'corecluster.algorithms.node.simple'
-STORAGE_SELECT_ALGORITHM = 'corecluster.algorithms.storage.simple'
-ID_GENERATOR = 'corecluster.algorithms.id.uuid_gen'
-AUTH_METHOD = 'corecluster.algorithms.auth.db'
-
 API_STATS = False
 INSTALLATION_ID = '_INSTALLATION_ID'
 
-######################################
-# Extensions                         #
-######################################
 APPS = [
     'corecluster.app',
+    'corecluster-auth-db.app',
+    'corecluster-algorithm-storage-default.app',
+    'corecluster-algorithm-node-default.app',
+    'corecluster-algorithm-id-uuid.app',
+    'corecluster-storage-libvirt.app',
     'corenetwork.app',
     'coredhcp.app',
     'coretalk.app',
--- scripts/core/corenetwork_config.py
+++ scripts/core/corenetwork_config.py
@@ -1,18 +1,11 @@
-USE_AUTODISCOVER = False
-CORE_URL = "http://192.168.27.254:8001/"
-
+USE_AUTODISCOVER = True
+CORE_URL = "http://localhost:8001/"
 INTERFACES = [
     {'iface':   'eth1',
      'cost':    100},
 ]
 VXLAN_INTERFACE='eth1'
 VXLAN_MULTICAST='239.0.0.1'
-NETWORK_ROUTED_DRIVER = 'corenetwork.drivers.network_quagga'
-NETWORK_ISOLATED_DRIVER = 'corenetwork.drivers.network_vxlan'
-CORE_DRIVER = 'corenetwork.drivers.core_default'
-NODE_DRIVER = 'corenetwork.drivers.node_default'
-VM_DRIVER = 'corenetwork.drivers.vm_default'
-
 QUAGGA_PASSWORD = "_QUAGGA_PASSWORD"
 OSPF_TOKEN = '_OSPF_TOKEN'
 IFACE_NAME_LENGTH = 15
